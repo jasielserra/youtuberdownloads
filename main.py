a78b9572d72840a3ae3d2b391ad12a18518d8002
Jasiel <jasiel_serra@hotmail.com>
2022-07-07 20:49:58 -0300

    Add Banner Aplication

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ class Downloader:
         self.window.title("Youtube Download")
         self.window.resizable(0, 0)
         self.window.geometry("1280x720+300+200")
+
+
+        self.img_logo = PhotoImage(file="assets/logo.png")
+
+        self.frame = Frame(self.window, bg="#3b3b3b", pady=80)
+        self.frame.pack(fill="x")
+
+        self.label_logo = Label(self.frame, image=self.img_logo, bg="#3b3b3b")
+        self.label_logo.pack()
+
         self.window.mainloop()
 
 Downloader()
