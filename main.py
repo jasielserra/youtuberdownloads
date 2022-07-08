334a908bbf8b33402b341e3d86149a0ac8a766e9
Jasiel <jasiel_serra@hotmail.com>
2022-07-07 22:16:16 -0300

    Using youtube_dl

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import youtube_dl
+import os
 
 class Downloader:
     def __init__(self):
@@ -8,6 +10,9 @@ class Downloader:
         self.window.geometry("1280x720+300+200")
 
         self.img_logo = PhotoImage(file="assets/logo.png")
+
+        self.audio = False
+        self.video = False
 
         self.frame = Frame(self.window, bg="#3b3b3b", pady=80)
         self.frame.pack(fill="x")
@@ -29,11 +34,42 @@ class Downloader:
         self.frame3 = Frame(self.window)
         self.frame3.pack()
 
-        self.radio1 = Radiobutton(self.frame3, text="Audio", value=0, command=None).pack(side="left")
-        self.radio2 = Radiobutton(self.frame3, text="Video", value=1, command=None).pack(side="left")
-        self.radio3 = Radiobutton(self.frame3, text="Audio e Video", value=2, command=None).pack(side="left")
-
+        self.radio1 = Radiobutton(self.frame3, text="Audio", value=0, command=self.validate_audio).pack(side="left")
+        self.radio2 = Radiobutton(self.frame3, text="Video", value=1, command=self.validate_video).pack(side="left")
+        self.radio3 = Radiobutton(self.frame3, text="Audio e Video", value=2, command=self.validate_all).pack(side="left")
 
         self.window.mainloop()
+
+    def validate_audio(self):
+        self.audio = True
+        self.video = False
+
+
+    def validate_video(self):
+        self.audio = False
+        self.video = True
+
+
+    def validate_all(self):
+        self.audio = False
+        self.video = False
+
+    def download(self, link):
+        os.system("youtube-dl " + str(link)) # Faz download do video.
+        self.complete()
+
+    def complete(self):
+        window = Toplevel()
+        window.title('Efetuado')
+        window.resizable(0, 0)
+        window.geometry('300x200')
+
+        text = Label(window, text='Download Efetuado', pady=30)
+        text.pack
+
+        button_exit = Button(window, text='OK', command=window.destroy)
+        button_exit.pack()
+
+
 
 Downloader()
