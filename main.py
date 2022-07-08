006a67cc9d24f8541aa741b15d7e0b2e458c9611
Jasiel <jasiel_serra@hotmail.com>
2022-07-07 21:22:19 -0300

    Entry Field for link

--- main.py
+++ main.py
@@ -16,6 +16,18 @@ class Downloader:
         self.label_logo = Label(self.frame, image=self.img_logo, bg="#3b3b3b")
         self.label_logo.pack()
 
+        self.frame2 = Frame(self.window, pady=20)
+        self.frame2.pack()
+
+        self.label_insert = Label(self.frame2, text="  Insert link:  ", font="arial 12")
+        self.label_insert.pack(side="left")
+
+        self.link = Entry(self.frame2, font="arial 20", width=50)
+        self.link.pack(side="left")
+
+        self.play = Button(self.frame2, bg="red", text=">", bd=0, fg="white", width=4, height=2, command=None)
+        self.play.pack()
+
         self.window.mainloop()
 
 Downloader()
